1d1bc92fd494690dca0032e215974f3cf0226bc8
2003devansh <faujdardevansh@gmail.com>
2024-03-08 13:44:22 +0530

    nothing spcific changes

--- amy-app/app/page.js
+++ amy-app/app/page.js
@@ -1,19 +1,18 @@
 "use client"
 
 import React, { useState } from 'react'
+import Header from '@/Components/Header';
 
 const page = () => {
-  const {marks ,setMarks} = useState(80);
-  const {telly,setTelly} = useState(78);
+   const [marks, setMarks] = useState(67); 
+
   return (
     <>
-    <h1>hi marks is {marks} </h1>
-    <button  onClick={()=>{
-      changeMarks();
-    }}>
-      click me 
-      
-    </button>
+    <h1>my marks is {marks}</h1>
+    <button onClick={()=>{
+      setMarks(78);
+    }}>click me </button>
+    <Header />
     </>
   )
 }
